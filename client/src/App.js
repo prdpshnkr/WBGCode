d4db9a168b946c5673bd8248a6e03c45b4f66ed3
prdpshnkr <prdpifs@gmail.com>
2022-02-10 12:42:39 +0530

    basic query from react

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
+import axios from "axios";
+
 function App() {
+  const handleAddUser = () => {
+    const userData = {
+      email:'abcd@gmail.com',
+      password: 'Password'
+    }
+
+    axios({
+      url: 'http://localhost:5000/graphql',
+      method: 'POST',
+      data: {
+        query:`
+          mutation {
+            addUser(userInput: {
+              email: "${userData.email}"
+              password: "${userData.password}"
+            }){
+              _id
+              email
+              password
+            }
+          }
+        `
+      }
+    }).then(res => {
+      console.log(res.data);
+    }).catch(err=>{
+      console.log(err);
+    })
+  } 
+
   return (
     <div className="App">
-      Hello
+      <button onClick={handleAddUser}>
+        Add User
+      </button>
     </div>
   );
 }
